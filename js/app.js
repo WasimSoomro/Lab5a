a32d62c1b8384da462e72f8eb5b5fdd322df000d
WASIM SOOMRO <soomrowasimw@gmail.com>
2023-05-08 00:49:16 -0600

    Submitting stuck at 4

--- js/app.js
+++ js/app.js
@@ -81,12 +81,19 @@ Test this function by hand in the console to get it working, and when you think 
 let testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
+let sum = 0;
+for (i=0; i < sum.length; i++) {
+  sum = sumArr[i]
+}
+return sum;
+console.log(sum);
 
+let stringArray = `${a},${b},${c} was passed in as an array of numbers, and ${sumOfArray} is their sum.`
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
